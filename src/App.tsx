057e0828b6e239e2db82ecebe0cb939fa365c22c
LuizGarbini <garbasneto@gmail.com>
2023-04-18 12:54:35 -0300

    :wrench: theme configuration styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { useState } from "react";
+import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant="primary"></Button>
       <Button variant="secondary"></Button>
       <Button variant="success"></Button>
       <Button variant="danger"></Button>
       <Button></Button>
-    </>
+    </ThemeProvider>
   );
 }
