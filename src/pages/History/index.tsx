223020031f406a22616cd9cefdd72fb4ed730a25
LuizGarbini <garbasneto@gmail.com>
2023-04-28 00:47:23 -0300

    :wrench: aprimorating inputs

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,11 @@
+import { HistoryContainer, HistoryList } from './styles'
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList></HistoryList>
+    </HistoryContainer>
+  )
+}
